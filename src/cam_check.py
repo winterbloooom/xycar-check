46c39b52624653107b5456ce24508156769e6df1
EunGi Han <717lumos@gmail.com>
2022-05-16 21:33:09 +0900

    fix errors in cam_test

--- src/cam_check.py
+++ src/cam_check.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
+#-*- coding:utf-8 -*-
 
 import rospy
 import cv2
+import numpy as np
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 
-image = None
+image = np.array([])
 bridge = CvBridge()
 
 def img_callback(msg):
@@ -18,15 +19,22 @@ rospy.Subscriber("/usb_cam/image_raw/", Image, img_callback)
 
 rate = rospy.Rate(10)
 
-while cv2.waitKey(1) != 27: #not rospy.is_shutdown():
-    for row in range(20, 481, 20):
-        # TODO 글자 위치 조정
-        cv2.putText(image, row, (2, row + 5), color=(255, 0, 0))
-        cv2.line(image, (5, row), (640, row), (255, 0, 0), 1)
+font =  cv2.FONT_HERSHEY_SIMPLEX
 
-    for col in range(20, 641, 20):
-        cv2.putText(image, col, (col - 5, 5), color=(255, 0, 0))
-        cv2.line(image, (col, 5), (col, 480), (255, 0, 0), 1)
+while cv2.waitKey(1) != 27: #not rospy.is_shutdown():
+    while not image.size == (640 * 480 * 3):
+        continue
+
+    for row in range(50, 481, 50):
+        # TODO 글자 위치 조정
+        cv2.putText(image, str(row), (2, row + 3), font, 0.3, (200, 0, 0), 1, cv2.LINE_AA)
+        cv2.putText(image, str(row), (615, row + 3), font, 0.3, (200, 0, 0), 1, cv2.LINE_AA)
+        cv2.line(image, (25, row), (610, row), (255, 0, 0), 1)
+
+    for col in range(50, 641, 50):
+        cv2.putText(image, str(col), (col - 10, 10), font, 0.3, (200, 0, 0), 1, cv2.LINE_AA)
+        cv2.putText(image, str(col), (col - 10, 473), font, 0.3, (200, 0, 0), 1, cv2.LINE_AA)
+        cv2.line(image, (col, 15), (col, 465), (255, 0, 0), 1)
 
     cv2.imshow("image", image)
     rate.sleep()
